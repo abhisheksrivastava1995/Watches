aad4f3e1da52f3c6c50ace5eace4db5d6edbe79d
abhisheksrivastava1995 <abhisheksrivastava3444@gmail.com>
2024-09-03 07:49:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+// Carousel function
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} 
+
+// Map and marker latitude and longitude definition
+
+var map = L.map('map').setView([-23.5113, -46.8768], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([-23.5113, -46.8768]).addTo(map);
+
+marker.bindPopup("<b>Rua Sem Nome - nº 00</b><br>Barueri - SP").openPopup();
